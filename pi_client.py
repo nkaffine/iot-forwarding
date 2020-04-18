ab6b395ca295ca3c9258e6d800c6738d96666441
Nicholas Kaffine <nicholas.kaffine@gmail.com>
2020-04-18 12:09:58 -0400

    adding pi client code

---
+++ pi_client.py
@@ -0,0 +1,20 @@
+import receiver
+
+class PiClient:
+    def __init__(self):
+        f = open("config.txt", "r")
+        self.server_ip = f.readline()
+        self.server_port = f.readline()
+        self.receiver = receiver.Receiver(self.server_ip, self.server_port)
+
+    def start(self):
+        while True:
+            self.receiver.initiateConnection()
+            try:
+                self.receiver.process()
+            except ConnectionResetError:
+                print("connection lost")
+
+if __name__ == "__main__":
+    client = PiClient()
+    client.start()
